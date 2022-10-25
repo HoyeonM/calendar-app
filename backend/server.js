7460f38a524071e68d5c59d6a04e03a52ae12749
Hoyeon Moon <mhy991011@gmail.com>
2022-10-25 16:44:33 -0700

    commit master

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3001; // <- 3000에서 다른 숫자로 변경
+const port = 3001; 
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mysql = require("mysql"); // << 새로 추가된 부분
+const mysql = require("mysql");
 
 // var connection = mysql.createConnection({
 //     /// 새로 추가된 부분
