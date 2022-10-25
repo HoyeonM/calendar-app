70e6aec4859c0907f6060eb70428f8f95d7b6628
HoyeonM <99692392+HoyeonM@users.noreply.github.com>
2022-10-25 16:54:57 -0700

    Add files via upload

---
+++ src/notification.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Notification(){
+    return (
+        <button class='headerbtn'>
+             <span class="material-symbols-outlined">notifications</span> 
+        </button>
+    )
+}
+export default Notification;
