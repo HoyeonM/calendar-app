3f0eba38c81131078f6c2885f7d54ff486ec0037
AudrisG <37347664+AudrisG@users.noreply.github.com>
2022-11-08 21:26:42 -0800

    Update todoForm.js

--- frontend/src/todoForm.js
+++ frontend/src/todoForm.js
@@ -1,86 +1,88 @@
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+//import Calendar from 'react-calendar';
 import { useState } from 'react';
-import { Editor } from "react-draft-wysiwyg";
+import "./App.css";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-
-import './App.js';
-import './App.css';
+import Axios from 'axios';
 
 function TodoForm() {
-    const [date, setDate] = useState(new Date());
-    const [todo, setTodo] = useState("");
-    const [dueDate, setDueDate] = useState("");
-    //const [todoInput, setTodoInput] = useState("");
-    //const todos = [];
-
-    document.addEventListener("DOMContentLoaded", () => {
-        const SAVING_MESSAGE = "Saving...";
-        const SAVED_MESSAGE = "All changes saved.";
-      
-        document
-          .querySelectorAll(".autosave-message")
-          .forEach((el) => (el.textContent = SAVED_MESSAGE));
-      
-        document.querySelectorAll("[data-autosave-value]").forEach((inputField) => {
-          inputField.addEventListener("change", async () => {
-            const name = inputField.getAttribute("name");
-            const value = inputField.value;
-            const autosaveMessageEl = inputField
-              .closest(".container")
-              .querySelector(".autosave-message");
-            const formData = new FormData();
-      
-            formData.append(name, value);
-            autosaveMessageEl.classList.add("autosave-message--saving");
-            autosaveMessageEl.textContent = SAVING_MESSAGE;
-      
-            const response = await fetch(value, {
-              method: "POST",
-              body: formData
-            });
-      
-            autosaveMessageEl.classList.remove("autosave-message--saving");
-            autosaveMessageEl.textContent = SAVED_MESSAGE;
-          });
+   const [id,setID] = useState("");
+   const [title, setTitle] = useState("");
+   const [entry, setEntry] = useState("");
+   const [dueDate, setDueDate] = useState("");
+   //sconst [checklist, setChecklist] = useState("");
+   const [checklistArray, setChecklistArray] = useState([]);
+   
+    const addChecklist = () => {
+      Axios.post("http://localhost:3306/insertChecklist", {
+         id: id,
+         title: title, 
+         entry: entry, 
+         dueDate: dueDate,
+       })
+       .then(() => {
+         console.log("Checklist successfully added!");
+       })
+      }
+   
+      const updateChecklist = () =>{
+         Axios.put("http://localhost:3306/updateChecklist", { 
+            id:id,
+            title: title, 
+            entry: entry, 
+            dueDate: dueDate,
+        }).then(() => {
+            console.log("Checklist successfully added!");
         });
-      }); 
-
-    const submitTodo = (event) => {
-        event.preventDefault();
-    }
+      }
+   
+      const deleteChecklist = (id) => {
+         let answer = window.confirm("Are you sure want to delete?");
+           if (answer) {
+             Axios.delete(`http://localhost:3306/deleteChecklist/${id}`).then((response) => {
+               setChecklistArray(
+                   checklistArray.filter((val) => {
+                       return val.id !== id;
+                   })
+               )
+             });
+           }
+       }
+   
     return (
-        <div>
-            <main>
-                <form onSubmit = {submitTodo}>
-                    <label for = "todo">Checklist Title:</label>
-                    <input
-                    id= "todo" 
-                    type = "text"
-                    name = "text"
-                    onChange = {event => setTodo(event.target.value)}
-                    />
-                    <input type = "submit" value = "submit" />
-                </form>
-            </main>
-            <span classname = 'bold'>Checklist Entry:</span>
-            <Editor
-                //editorState = {editorState}
-                toolbarClassName="toolbarClassName"
-                wrapperClassName="wrapperClassName"
-                editorClassName="editorClassName"
-                wrapperStyle={{ width: 500, height: 300, border: "1px solid black" }}
-                //onEditorStateChange = {this.onEditorStateChange}      
-            />
-            <span classname = 'bold'>Checklist Due Date:</span>
-            <input 
-            type = "text"
-            placeholder={date.toDateString()}
-            onChange={event => setDueDate(event.target.value)}
-            />
-        <button>submit</button>
-        </div> 
-    )  
+      <div class = "todo">
+        <div class = "todo_sidebar">
+          <button class = "todo_save"
+            onClick = {addChecklist} 
+            type = "button">Save Checklist</button>
+          {/*
+          <button class="checklist__update"
+            onClick={updateChecklist}  
+            type="button">Update Checklist</button>
+          */}
+          <button class = "todo_delete"
+            onClick = {deleteChecklist}
+            type = "button">Delete Checklist</button>
+        </div>
+        <div class = "todo_preview">
+          <label for = "todo">Checklist Title:</label>
+            <input class = "todo_title"
+              type = "text"
+              placeholder = {title}
+              onChange={(event) =>{
+                setTitle(event.target.value);}}>
+            </input>
+            <textarea class = "todo_body"
+              rows = {10}
+              cols = {175}
+              onChange = {(event) => {
+                setEntry(event.target.value);
+                }}
+              placeholder = "Enter Checklist Entry Here">
+            </textarea>
+        </div>
+      </div>  
+    );  
  } 
 
 export default TodoForm;
