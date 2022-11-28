dd5d4c516af7ce34c43ba1780251d01f8635e743
tylerdvanl <49462625+tylerdvanl@users.noreply.github.com>
2022-11-27 16:29:38 -0800

    Revert "Update todoForm.js"
    This reverts commit f69d553a7c8e7e0286d6bbe9516e2192f454effb.

--- frontend/src/todoForm.js
+++ frontend/src/todoForm.js
@@ -3,20 +3,19 @@ import Axios from 'axios';
 import './App.css';
 import { v4 } from 'uuid'
 
+
+
 function TodoForm() {
     const [currentTodo, setCurrentTodo] = useState("");
     const [todos, setTodos] = useState([]);
     const [dateTime, setDate] = useState("");
 
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  
     useEffect ( () => {
       getTodos();
 }, []);
 
     const getTodos = ()=>{
-      console.log("Trying to get todos")
+      console.log("trying to get todos")
       Axios.get("http://localhost:3307/getTodos").then((res) => {
         console.log(res.data)
         setTodos(res.data);
@@ -25,48 +24,43 @@ function TodoForm() {
   
     const addTodo = () => {
       Axios.post("http://localhost:3307/insertTodo", {
-        todo: currentTodo,
-        dateTime: dateTime, 
+        todo: currentTodo, 
         isCompleted: false, 
       })
      .then(res => {
         console.log("Successful todo add!");
-        window.alert("Saved Checklist Successfully!");
         getTodos();
       })
    }
 
    const deleteTodoDB = (id) => {
   
-      Axios.delete(`http://localhost:3307/deleteTodo/${id}`)
-      .then((res)=>{
+      Axios.delete(`http://localhost:3307/deleteTodo/${id}`).
+      then((res)=>{
         console.log(res.data);
         console.log("todo deleted!");
-        window.alert("Deleted Checklist Succesfully!");
       })
+    
   }
 
-  const completeTodoDB = (id, status, todo, dateTime)=>{
+  const completeTodoDB = (id,status)=>{
     console.log("trying to update todos")
     //console.log(status)
     Axios.put(`http://localhost:3307/updateTodo`, {
       id: id,
-      status: status,
-      todo: todo,
-      dateTime: dateTime
+      status: status
     })
     .then((res) => {
-      console.log("Updated Todo")
-      window.alert("Checklist Updated!");
+      console.log("updated todo")
     })
+   // getTodos();
   }
 
     function createNewTodo(currentTodo) {
       let todosArray = [...todos];
       todosArray.push({
         todo: currentTodo,
-        isCompleted: false,
-        dateTime: dateTime
+        isCompleted: false
       });
       setTodos(todosArray);
       getTodos();
@@ -89,43 +83,33 @@ function TodoForm() {
     }
   
     return (
-      <div>
+      <div >
         <input
           className="todo-input"
           value={currentTodo}
-          placeholder="What needs to get done?"
           onChange={e => {
             setCurrentTodo(e.target.value);
-          }}
-        />
-        <input
-          className="todoDueDate-input"
-          value={dateTime}
-          placeholder= {date}
-          onChange={e => {
-            setDate(e.target.value);
           }}
           onKeyPress={e => {
             if (e.key === "Enter") {
               createNewTodo(currentTodo);
               setCurrentTodo("");
-              setDate("");
-            }}}
+            }
+          }}
+          placeholder="What needs to get done?  Press Enter to submit..."
         />
         <div className="alltodos">
         {todos.map((todo, index) => (
-                  <div key={v4()} className="todo" id = {todo.todo}> 
-                  <div key={v4()} className="todoDueDate" id = {todo.dateTime}>
-                  <div key={v4()} className="checkbox" onClick={() => completeTodo(index)}>
-                     {todo.isCompleted && <span>&#x2714;</span>}
-                  </div>
-                    <div key={v4()} className={todo.isCompleted ? "done" : ""}>{todo.todo}</div>
-                    <div key={v4()} className="delete" onClick={() => deleteTodo(index)}>
+                <div key={v4()} className="todo" id = {todo.dateTime}>
+                    <div key={v4()} className="checkbox" onClick={() => completeTodo(index)}>
+                    {todo.isCompleted && <span>&#x2714;</span>}
+                    </div>
+                    <div key={v4()}   className={todo.isCompleted ? "done" : ""}>{todo.todo}</div>
+                    <div key={v4()}  className="delete" onClick={() => deleteTodo(index)} >
                     &#128465;
                     </div>
                 </div>
-              </div>
-
+        
         ))}
           </div>
         {todos.length > 0 && `${todos.length} items`}
